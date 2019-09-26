6063be2edcb9ed6194d8f151d73b2e0feb813673
Artyom <artyom.tsaplin@gmail.com>
2019-09-26 21:40:33 +0300

    Permit code for frames.

--- app/admin/frames.rb
+++ app/admin/frames.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Frame do
 
-  permit_params :title, :content, :is_final, :is_success, :is_check_point, :survey_id
+  permit_params :title, :content, :is_final, :is_success, :is_check_point, :survey_id, :code
 
 end
